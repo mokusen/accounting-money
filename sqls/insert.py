17e71df1cb87618eeb826d91c3e05fb4365842e0
mokusen <kikawa.tech@gmail.com>
2018-12-02 16:52:15 +0900

    add time create method

--- sqls/insert.py
+++ sqls/insert.py
@@ -1,14 +1,25 @@
 import sqlite3
 import os
 from contextlib import closing
+from datetime import datetime
 
 path = os.getcwd()
 dbpath = path + '\data.db'
 detect_types = sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES
 
+def _add_time(insert_list):
+    insert_list.append(datetime.now())
+    insert_list.append(datetime.now())
+    insert_list = [tuple(insert_list)]
+    return insert_list
+
 def insert_base(insert_list):
     with closing(sqlite3.connect(dbpath,detect_types=detect_types)) as conn:
         c = conn.cursor()
+
+        # 作成時間と更新時間を追加する
+        insert_list = _add_time(insert_list)
+        print(insert_list)
 
         # executeメソッドでSQL文を実行する
         sql = 'insert into base (name, create_ts, update_ts) values (?,?,?)'
@@ -19,6 +30,10 @@ def insert_base(insert_list):
 def insert_accounting(insert_list):
     with closing(sqlite3.connect(dbpath,detect_types=detect_types)) as conn:
         c = conn.cursor()
+
+        # 作成時間と更新時間を追加する
+        insert_list = _add_time(insert_list)
+        print(insert_list)
 
         # executeメソッドでSQL文を実行する
         sql = 'insert into accounting (money, use, year, month, day, create_ts, update_ts) values (?,?,?,?,?,?,?)'
