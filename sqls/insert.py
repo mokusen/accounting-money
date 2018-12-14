c698ab21f3128ae5cddad238b4fe24c7acb03cf5
mokusen <kikawa.tech@gmail.com>
2018-12-15 00:06:01 +0900

    create common parts of sql and separated

--- sqls/insert.py
+++ sqls/insert.py
@@ -1,18 +1,11 @@
 import sqlite3
 import os
 from contextlib import closing
-from datetime import datetime
+from . import common
 
 path = os.getcwd()
 dbpath = path + '\data.db'
 detect_types = sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES
-
-
-def _add_time(insert_list):
-    insert_list.append(datetime.now())
-    insert_list.append(datetime.now())
-    insert_list = [tuple(insert_list)]
-    return insert_list
 
 
 def insert_base(insert_list):
@@ -20,7 +13,8 @@ def insert_base(insert_list):
         c = conn.cursor()
 
         # 作成時間と更新時間を追加する
-        insert_list = _add_time(insert_list)
+        insert_list = common._insert_add_time(insert_list)
+        insert_list = common.__type_change_sqlValue(insert_list)
 
         # executeメソッドでSQL文を実行する
         sql = 'insert into base (name, create_ts, update_ts) values (?,?,?)'
@@ -34,7 +28,8 @@ def insert_accounting(insert_list):
         c = conn.cursor()
 
         # 作成時間と更新時間を追加する
-        insert_list = _add_time(insert_list)
+        insert_list = common._insert_add_time(insert_list)
+        insert_list = common.__type_change_sqlValue(insert_list)
 
         # executeメソッドでSQL文を実行する
         sql = 'insert into accounting (use, money, year, month, day, create_ts, update_ts) values (?,?,?,?,?,?,?)'
@@ -46,6 +41,8 @@ def insert_accounting(insert_list):
 def insert_cache(insert_list):
     with closing(sqlite3.connect(dbpath, detect_types=detect_types)) as conn:
         c = conn.cursor()
+
+        insert_list = common.__type_change_sqlValue(insert_list)
 
         # executeメソッドでSQL文を実行する
         sql = 'insert into cache (use, min_money, max_money, min_year, max_year, min_month, max_month, min_day, max_day) values (?,?,?,?,?,?,?,?,?)'
