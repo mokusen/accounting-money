65ac5ffd1806f297fa9f3037deae0fbe87bc7a4a
mokusen <kikawa.tech@gmail.com>
2018-12-23 14:41:21 +0900

    add dbInit method before publishing

--- datebase_init.py
+++ datebase_init.py
@@ -10,21 +10,21 @@ create.create_accounting()
 create.create_cache()
 
 # 初期メタデータ取得
-use_list = useListCreate.create_init_list()
+# use_list = useListCreate.create_init_list()
 
 # 初期メタデータ挿入
-for item in use_list:
-    insert.insert_base(item)
+# for item in use_list:
+#     insert.insert_base(item)
 
 # 初期cache情報挿入
-insert.insert_cache(['', '', '', '', '', '', '', '', ''])
+# insert.insert_cache(['', '', '', '', '', '', '', '', ''])
 
 # 挿入されているメタデータを検索
-meta_list = select.select_base()
+# meta_list = select.select_base()
 
 # 初期課金履歴取得
-accounting_list = accountingListCreate.create_list()
+# accounting_list = accountingListCreate.create_list()
 
 # 初期課金履歴挿入
-for item in accounting_list:
-    insert.insert_accounting(item)
+# for item in accounting_list:
+#     insert.insert_accounting(item)
